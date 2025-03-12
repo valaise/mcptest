4d454ee511b7b8eab60ebf809f43cea774371026
valaise <valaise@Mac.fritz.box>
2025-03-12 20:00:25 +0100

    Fix: Frontend-Build-Probleme beheben

---
+++ todo-app/frontend/webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "fs": false,
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve("buffer/")
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]
+}; 
